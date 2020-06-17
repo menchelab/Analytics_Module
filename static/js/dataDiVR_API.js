e2aa2f31ce01b4b13574b12c5d10288dfeb50fb9
Eugenia Iofinova <eiofinova@cemm.oeaw.ac.at>
2020-06-17 09:34:37 +0200

    switch back to server for data

--- static/js/dataDiVR_API.js
+++ static/js/dataDiVR_API.js
@@ -1,6 +1,6 @@
 ///////GLOBAL VARS vvvvvvv
 var dbprefix = 'http://asimov.westeurope.cloudapp.azure.com:8887';
-var dbprefix = ""
+// var dbprefix = ""
 //create the global ue4(...) helper function
 "object"!=typeof ue||"object"!=typeof ue.interface?("object"!=typeof ue&&(ue={}),ue.interface={},ue.interface.broadcast=function(e,t){if("string"==typeof e){var o=[e,""];void 0!==t&&(o[1]=t);var n=encodeURIComponent(JSON.stringify(o));"object"==typeof history&&"function"==typeof history.pushState?(history.pushState({},"","#"+n),history.pushState({},"","#"+encodeURIComponent("[]"))):(document.location.hash=n,document.location.hash=encodeURIComponent("[]"))}}):function(e){ue.interface={},ue.interface.broadcast=function(t,o){"string"==typeof t&&(void 0!==o?e.broadcast(t,JSON.stringify(o)):e.broadcast(t,""))}}(ue.interface),(ue4=ue.interface.broadcast);
 ////  API DEFENITION
