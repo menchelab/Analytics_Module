0151890b7eb461460c7e0e6a12d08ffea1513e5f
martin c <norbert.unfug@gmail.com>
2020-12-16 13:03:06 +0100

    repaired rundataserver.ps1

--- runDataSerVR.ps1
+++ runDataSerVR.ps1
@@ -8,5 +8,5 @@ venv\Scripts\activate
 python -m pip install -r requirements.txt
 $env:FLASK_ENV="development"
 $env:FLASK_APP="app.py"
-flask run --port 1337
-
+#flask run --port 1337
+python app.py
