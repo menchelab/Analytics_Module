c19c5c35cc9f623d3a9608f355b1c9745b51fa15
sepppirch <sepppirch@gmail.com>
2020-12-16 13:34:53 +0100

    added virtual env stuff to the runDataServer.ps1 script

--- runDataSerVR.ps1
+++ runDataSerVR.ps1
@@ -2,6 +2,8 @@ $host.ui.RawUI.WindowTitle = 'DataSerVR'
 $host.UI.RawUI.BackgroundColor='darkgray'
 
 Clear-Host
+#create virtual env in windows and activate it and install requirements.txt
+py -3 -m venv venv
 venv\Scripts\activate
 #pip install flask_cors
 #pip install pymysql
