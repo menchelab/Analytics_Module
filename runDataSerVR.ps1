7a072f9658c0f163b754bcb1fd5c0be16533e49d
martin c <norbert.unfug@gmail.com>
2020-12-16 12:57:52 +0100

    added requirents.txt to the runDataServer.ps1 script to make it install all dependencies when running on windows

--- runDataSerVR.ps1
+++ runDataSerVR.ps1
@@ -3,8 +3,9 @@ $host.UI.RawUI.BackgroundColor='darkgray'
 
 Clear-Host
 venv\Scripts\activate
-pip install flask_cors
-pip install pymysql
+#pip install flask_cors
+#pip install pymysql
+python -m pip install -r requirements.txt
 $env:FLASK_ENV="development"
 $env:FLASK_APP="app.py"
 flask run --port 1337
