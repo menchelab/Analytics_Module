54ae5341caa498b58087bf5c16b8c69a4179f04b
Celine Sin <celine.sin@gmail.com>
2021-02-01 17:19:48 +0100

    IP: sql cleaning script

---
+++ zNumbers/s20210201_cleanSQLdatabase.sql
@@ -0,0 +1,10 @@
+DROP SCHEMA IF EXISTS 
+airports, Bunny, coronavirus, Datadivr, Datadivr_jen, 
+Datadivr_jentest, Datadivr_meta, Datadivr_sessions, Datadivr_tmp, 
+dumb1, dumbproj, 
+Gene2Disease, Gene2Pathways, Gene2Tissue, GenesGO, 
+HumanPhenotypes, jentest10, jentest11, jentest12, 
+MeSH, MousePhenotypes, networks, 
+radipop, Results_Funclusters, soundr, speech, sys, test, 
+tmp_airports, tmp_Bunny, tmp_Datadivr_jen, tmp_dumbproj, tmp_jentest10, 
+tmp_jentest11, tmp_jentest12, tmp_null, tmp_ppi, tmp_test, tmp_yeast_ppi, yeast_ppi;
