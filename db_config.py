309756de62beafc37b2eacf869bbd3486217e40d
sepppirch <sepppirch@gmail.com>
2021-03-04 13:54:45 +0100

    added db_config.py :D

---
+++ db_config.py
@@ -0,0 +1,24 @@
+'''
+Database configurations for all DBs used for the DataDivr.
+
+INSTRUCTIONS:
+    - Copy this file to ./db_config.py
+    - Fill in the database passwords
+IMPORTANT: DO NOT put passwords in this file.
+'''
+
+# DATABASES
+
+vrnetzer = {  "host":     "vrnetzer.westeurope.cloudapp.azure.com",
+            'user':     'readonly',
+            'password':  'ra4Roh7ohdee',
+            'database': 'Vrnetzer'
+
+}
+
+
+
+
+
+# Update this variable to switch db used by the platform
+DATABASE = vrnetzer
